d00770d73e87da7f81ce485b5bf117952f0f0534
Hourigan <hourigan@hourigan-pc.(none)>
2013-04-02 14:49:54 +0200

    Default project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpensesManager::Application.config.secret_token = 'cec643c2e3c3a5a9b3b6cd5f90c5dfc19a584b6bc5429bd95291323c84cee489e940b24979cdacd330ca425234adf4ac992d80a887dc0d245fd7f78b3333d484'
